f1584d42ca68a4bf03ac3ede7d07cbc9f8772985
YavaDeus <36574502+YavaDeus@users.noreply.github.com>
2018-02-22 05:47:14 +0100

    Add files via upload

--- addButton.js
+++ addButton.js
@@ -1,5 +1,50 @@
 //chrome.extension.sendRequest( function () {
 	var monElement = $(".group_icon");
 	monElement.append('<button>Click Me!</button>');
+	
+	//<script src="data.json"></script>
+	
+	//var data = require('./data.json');
+	//var data = $.getJSON(chrome.extension.getURL("manifest.json"));
+	//var data = JSON.parse(chrome.extension.getURL("data.json"));
+	
+	/*chrome.extension.getPackageDirectoryEntry(function(root) {
+		root.getFile("data.json", {}, function(fileEntry) {
+			fileEntry.file(function(file) {
+				var reader = new FileReader();
+				reader.onloadend = function(e) {
+					var data = JSON.parse(this.result);
+					
+				};
+				reader.readAsText(file);
+			});
+		});
+	});*/
+	
+	/*var xhr = new XMLHttpRequest;
+	xhr.open("GET", chrome.runtime.getURL("data.json"));
+	xhr.onreadystatechange = function() {
+	  if (this.readyState == 4) {
+		console.log("request finished, now parsing");
+		window.json_text = xhr.responseText;
+		console.log(xhr.responseText);
+		window.parsed_json = JSON.parse(xhr.responseText);
+		console.log("parse results:");
+		console.dir(window.parsed_json);
+	  }
+	};
+	xhr.send();*/
+	
+localStorage['links'] = [
+		"Genius Editor Helper"
+	];
+	
+	var data=localStorage.getItem('links');  //récupérer la valeur
+	/*localStorage.setItem(clef,valeur);  //donner une nouvelle valeur
+	localStorage.removeItem(clef);  //supprimer l'item
+	var clef=localStorage.key(n);  //récupérer la clef du n item.
+	localStorage.clear();  //vider localStorage
+	localStorage.length();  //le nombre de clef de localStorage*/
+
 //});
-console.log("oti");
+console.log(data);
