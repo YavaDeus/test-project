52ce17a8efdb56f2eb61048af7974f864e2ac363
YavaDeus <36574502+YavaDeus@users.noreply.github.com>
2018-02-25 16:11:49 +0100

    Add files via upload

--- init.js
+++ init.js
@@ -1,30 +1,129 @@
 "use strict";
-function init()
-{
-	localStorage['links'] = [
-		"Genius Editor Helper 2", "toto"
-	];
-	console.log("test 1");
+
+//function init()
+//{
+//	localStorage['links'] = [
+//		"Genius Editor Helper 2", "toto"
+//	];
+//	console.log("test 1");
+//}
+//console.log(localStorage['name']);
+//
+
+//function firstLoadTags() {
+//	chrome.storage.sync.get(['tags'], function (items) {
+//		if (!items.tags) {
+//			chrome.storage.sync.set({
+//				'tags': [{
+//						'name': 'gras',
+//						'value': '*'
+//					}, {
+//						'name': 'souligné',
+//						'value': '__m'
+//					}, {
+//						'name': 'italique',
+//						'value': 'i--'
+//					},
+//				]
+//			}, function () {
+//				console.log('Preload tags ok');
+//			});
+//		}
+//	});
+//
+//}
+
+function appendTags(tags) {
+	if (tags) {
+		var divTags = $("div.tags");
+		for (let item of tags) {
+			var textLigne = '<div class="ligne"><span>' + item.name + '</span></div>';
+
+			divTags.append(textLigne);
+		}
+	}
 }
-console.log(localStorage['name']);
 
-$("#nameButton").on("click", addTag);
+function listTags(preLoad, forceReload) {
+	console.log('List tags');
+	chrome.storage.sync.get(['tags'], function (items) {
+		appendTags(items.tags);
+		if ((preLoad && items.tags === undefined) || forceReload) {
+			chrome.storage.sync.set({
+				'tags': [{
+						'name': 'gras',
+						'value': '*'
+					}, {
+						'name': 'souligné',
+						'value': '__m'
+					}, {
+						'name': 'italique',
+						'value': 'i--'
+					}
+				]
+			}, function () {
+				console.log('Preload tags ok');
+				chrome.storage.sync.get(['tags'], function (items) {
+					appendTags(items.tags);
+				});
+			});
 
-function addTag()
-{
-	
+		}
 
-    
-	
-	var name = $("#nameButton").attr("name");
-	console.log("name");
-	console.log(name);
-	//localStorage['name'] = [name];
-	//localStorage['links'] = ["bob"];
-	
-	// Save it using the Chrome extension storage API.
-    chrome.storage.sync.set({'name1': name}, function() {
-      console.log('Settings saved');
-    });
-	
+	});
 }
+
+function appendActions(actions) {
+	if (actions) {
+		var divActions = $("div.actions");
+		for (let item of actions) {
+			var textLigne = '<div class="ligne"><a href="' + item.url + '" target="_blank">' + item.name + '</a></div>';
+
+			divActions.append(textLigne);
+		}
+	}
+}
+
+function listActions(preLoad, forceReload) {
+	console.log('List actions');
+
+	chrome.storage.sync.get(['actions'], function (items) {
+		appendActions(items.actions);
+		if ((preLoad && items.actions === undefined) || forceReload) {
+			chrome.storage.sync.set({
+				'actions': [{
+						'url': '//toto',
+						'name': 'Guide 1'
+					}, {
+						'url': '//titi',
+						'name': 'Guide 2'
+					}, {
+						'url': '//tete',
+						'name': 'iGuide 3'
+					}
+				]
+			}, function () {
+				console.log('Preload actions ok ');
+				chrome.storage.sync.get(['actions'], function (items) {
+					appendActions(items.actions);
+				});
+			});
+
+		}
+
+	});
+
+}
+
+function initLists() {
+	var preLoad = true;
+	var forceReload = false;
+	listTags(preLoad, forceReload);
+	listActions(preLoad, forceReload);
+	console.log(' Init done ');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	initLists();
+}, false);
+
---
+++ options.js
@@ -0,0 +1,28 @@
+"use strict";
+
+$("#nameButton").on("click", addTag);
+
+function addTag() {
+
+	var name = $("#nameButton").attr("name");
+	console.log("name");
+	console.log(name);
+	//localStorage[' name '] = [name];
+	//localStorage[' links '] = ["bob"];
+
+	// Save it using the Chrome extension storage API.
+	chrome.storage.sync.get(['tags'], function (items) {
+		var tags = items.tags;
+		tags.push({
+			'name': name,
+			'value': 'op--'
+		});
+
+		chrome.storage.sync.set({
+			'tags': tags
+		}, function () {
+			console.log('Settings saved ');
+		});
+	});
+
+}
