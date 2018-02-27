716c03c46e069a8c6e62e365c5c63375c1db9a9b
YavaDeus <36574502+YavaDeus@users.noreply.github.com>
2018-02-27 23:19:39 +0100

    Add files via upload

--- addButtons.js
+++ addButtons.js
@@ -4,12 +4,20 @@ function createList() {
 	var page = $("h1.header_with_cover_art-primary_info-title")[0];
 	var artist = $("a.header_with_cover_art-primary_info-primary_artist")[0];
 	var album = $("a[ng-bind='album.name']")[0];
-	var writtenby = $("expandable-list[label='Written By'] a");
-	var featuring = $("expandable-list[label='Featuring'] a");
+	var writtenby = $("expandable-list[label='Written By'] a:not([ng-click=\"ctrl.show_more()\"])");
+	var featuring = $("expandable-list[label='Featuring'] a:not([ng-click=\"ctrl.show_more()\"])");
+	var writers = $("expandable-list[label='Writers'] a:not([ng-click=\"ctrl.show_more()\"])");
+	
 
 	var lastPage = new ActionList();
 	if (page)
 		lastPage.add(new NavigationLink(0, page.innerText, 1, window.location.href, 'Titre'));
+	else
+	{
+		page = $("h1.profile_identity-name_iq_and_role_icon")[0];
+		if (page)
+			lastPage.add(new NavigationLink(0, page.firstChild.data, 1, window.location.href, 'Artiste'));
+	}
 	if (artist)
 		lastPage.addDedupe(new NavigationLink(0, artist.innerText, 2, artist.href, 'Artiste'));
 	if (album)
@@ -19,6 +27,13 @@ function createList() {
 		for (let item of writtenby)
 		{
 			lastPage.addDedupe(new NavigationLink(0, item.innerText, 2, item.href, 'Written by'));
+		}
+	}
+	if (writers.length > 0)
+	{
+		for (let item of writers)
+		{
+			lastPage.addDedupe(new NavigationLink(0, item.innerText, 2, item.href, 'Writers'));
 		}
 	}
 	if (featuring.length > 0)
